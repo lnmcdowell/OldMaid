f205ca5cd0d9ae59d1a893fc83cf7dd0f479294c
Larry Nathaniel Mcdowell <nathaniel29651@gmail.com>
2021-01-30 14:18:13 -0500

    Initial Commit

---
+++ OldMaid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OldMaid
+//
+//  Created by Larry Mcdowell on 1/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OldMaid/OldMaidApp.swift
@@ -0,0 +1,17 @@
+//
+//  OldMaidApp.swift
+//  OldMaid
+//
+//  Created by Larry Mcdowell on 1/30/21.
+//
+
+import SwiftUI
+
+@main
+struct OldMaidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
